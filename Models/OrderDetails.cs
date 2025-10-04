91fca8537123aaa6de121b5bc839f1e87f0e1fa5
Christina <christinadiamante113@gmail.com>
2025-10-05 00:06:29 +0800

    Add OrderDetails model

---
+++ Models/OrderDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Eatspress.Models
+{
+    public class OrderDetails
+    {
+        public int Order_Id { get; set; }
+        public int Item_Id { get; set; }
+        public int Quantity { get; set; }
+        public Order Order { get; set; } = null;
+        public FoodItem FoodItem { get; set; } = null;
+    }
+}
