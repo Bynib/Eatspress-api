07e22595a49fcd62cf44b92dea24b58245f82d22
Beirun <orillobernardjay@gmail.com>
2025-11-10 21:21:20 +0800

    Update UpdateUserRequest

--- ServiceModels/UpdateUserRequest.cs
+++ ServiceModels/UpdateUserRequest.cs
@@ -7,9 +7,8 @@
         public string Lastname { get; set; }
         public string Email { get; set; }
         public string Phone_No { get; set; }
-        public int Address_Id { get; set; }
-        public string OldPassword { get; set; }
-        public string NewPassword { get; set; }
-        public string ConfirmPassword { get; set; }
+        public string? OldPassword { get; set; }
+        public string? NewPassword { get; set; }
+        public string? ConfirmPassword { get; set; }
     }
 }
